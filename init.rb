0d3cad815fecc9e9e8f9f85e94b3ea7e605f7767
Myron Marston <myron.marston@gmail.com>
2010-03-13 10:16:29 -0800

    Use basic auth to administer the app.

--- init.rb
+++ init.rb
@@ -3,14 +3,11 @@ gem "data_objects", "= 0.10.0"
 require "integrity"
 
 Integrity.new(
-  # Uncomment to setup a password
-  # :use_basic_auth => true,
-  # :admin_username => "admin",
-  # :admin_password => "passwd",
+  :use_basic_auth => true,
+  :admin_username => "admin",
+  :admin_password => "passwd",
 
   :database_uri     => ENV["DATABASE_URL"],
   :export_directory => File.dirname(__FILE__) + "/tmp",
   :log => File.dirname(__FILE__) + "/tmp/integrity.log"
 )
-
-
