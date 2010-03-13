8605e3d57023fd4a55218f8ddee72ab9f0c61801
Myron Marston <myron.marston@gmail.com>
2010-03-13 10:55:21 -0800

    Use environment variables for the username and password.

--- init.rb
+++ init.rb
@@ -4,8 +4,8 @@ require "integrity"
 
 Integrity.new(
   :use_basic_auth => true,
-  :admin_username => "admin",
-  :admin_password => "passwd",
+  :admin_username => ENV["ADMIN_USER"],
+  :admin_password => ENV["ADMIN_PASSWORD"],
 
   :database_uri     => ENV["DATABASE_URL"],
   :export_directory => File.dirname(__FILE__) + "/tmp",
